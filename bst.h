fd01c1b3b1581da168cfa4697eab7fc03ef214a3
sticken88 <matteo.sticco@gmail.com>
2015-05-03 15:38:56 -0700

    Uodated prototype of 'in_order_successor'

--- bst.h
+++ bst.h
@@ -19,9 +19,9 @@ bst_node *search_data(bst_node *node, int data); //DONE
 bst_node *get_maximum(bst_node *node); // DONE
 bst_node *get_minimum(bst_node *node); // DONE
 bst_node *insert(bst_node *node, int data); // DONE
-bst_node *in_order_predecessor(bst_node *node, int data);
-bst_node *in_order_successor(int data);
+bst_node *in_order_predecessor(bst_node *node, int data); // DONE
+bst_node *in_order_successor(bst_node *node, int data);
 int *delete(int data);
-void bst_print(bst_node *node, int level);
+void bst_print(bst_node *node, int level); // DONE
 
 #endif
