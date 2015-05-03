6dc675c29dd318272856be8eff17ae9a9179d181
sticken88 <matteo.sticco@gmail.com>
2015-05-03 16:10:27 -0700

    Removed 'create_bst'. It was useless.

--- bst.h
+++ bst.h
@@ -12,15 +12,13 @@ typedef struct node
 }bst_node;
 
 // Functions prototypes
-
-bst_node *create_bst(int data); // DONE
 bst_node *create_node(int data); // DONE
 bst_node *search_data(bst_node *node, int data); //DONE
 bst_node *get_maximum(bst_node *node); // DONE
 bst_node *get_minimum(bst_node *node); // DONE
 bst_node *insert(bst_node *node, int data); // DONE
 bst_node *in_order_predecessor(bst_node *node, int data); // DONE
-bst_node *in_order_successor(bst_node *node, int data);
+bst_node *in_order_successor(bst_node *node, int data); // DONE
 int *delete(int data);
 void bst_print(bst_node *node, int level); // DONE
 
