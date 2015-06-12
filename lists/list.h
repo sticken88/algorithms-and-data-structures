aa1fdc2a21debdb5628a8be7f38d48ba35fe9bfd
sticken88 <matteo.sticco@gmail.com>
2015-06-12 16:01:39 +0200

    Changed prototypes.

--- lists/list.h
+++ lists/list.h
@@ -12,11 +12,12 @@ typedef struct l_node{
 
 
 list_node *create_node(float new_value); // DONE
-list_node *insert_node_head(list_node *head, list_node *tail, float new_value); // DONE
-list_node *insert_node_tail(list_node *tail, float new_value);
+void insert_node_head(list_node **head, list_node **tail, float new_value); // DONE
+void insert_node_tail(list_node *tail, float new_value);
 void delete_node();
 void reverse_list(list_node *node);
 void sort_list(list_node *node);
-void print_list(list_node *node); // DONE
+void print_list(list_node **node); // DONE
+void free_list(list_node *node);
 
 #endif
