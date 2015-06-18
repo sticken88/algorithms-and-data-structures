ae47460a14ce4d2aba787db716a67dc05764749c
Matteo Sticco <matteo.sticco@gmail.com>
2015-06-18 18:04:48 +0200

    Added function prototypes for 'insert_node_circular' and 'print_circular_list'

--- lists/list.h
+++ lists/list.h
@@ -14,10 +14,12 @@ typedef struct l_node{
 list_node *create_node(float new_value); // DONE
 void insert_node_head(list_node **head, list_node **tail, float new_value); // DONE
 void insert_node_tail(list_node **head, list_node **tail, float new_value); // DONE
+void insert_node_circular(list_node **head, list_node **tail, float new_value);
 void delete_node(list_node **head, float value);
 void reverse_list(list_node **head, list_node **tail); // DONE
 void sort_list(list_node **node);
 void print_list(list_node *node); // DONE
+void print_circular_list(list_node *node, list_node *tail);
 void free_list(list_node **node);
 
 #endif
