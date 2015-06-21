6d2a1f8ea46fc2b6c5892f761ab3055830aa8c85
Matteo Sticco <matteo.sticco@gmail.com>
2015-06-21 11:44:50 +0200

    Added functions signature for graph traversal and added a flag used to check whether a node has been visited or not.

--- graphs/graph.h
+++ graphs/graph.h
@@ -7,6 +7,7 @@
  */
 typedef struct l_node{
 	int value;
+	int visited;
     struct l_node *next;
 }list_node;
 
@@ -32,6 +33,10 @@ typedef struct g{
 graph *graph_init(int nodes); // DONE
 graph_node *create_graph_node(int new_value); // DONE
 list_node *create_list_node(int new_value); // DONE
-graph *graph_add_node(graph *gr, int node_index, int new_value);
+graph *graph_add_node(graph *gr, int node_index, int new_value); // DONE
+void depth_first_search(graph *gr);
+void breadth_first_search(graph *gr);
+void dijkstra(graph *gr);
+void bellman_ford(graph *gr);
 
 #endif
