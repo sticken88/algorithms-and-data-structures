98873cc94d0aaa13838138ccd9276f1cf79ef7c5
Matteo Sticco <matteo.sticco@gmail.com>
2015-06-23 11:32:38 +0200

    Redefined (again) the structure of a node belonging to a graph.

--- graphs/graph.h
+++ graphs/graph.h
@@ -3,22 +3,22 @@
 
 
 /*
- * list node struct definition
+ * graph node struct definition
  */
-typedef struct l_node{
+typedef struct g_node{
 	int value;
 	int visited;
-    struct l_node *next;
-}list_node;
+    struct g_node *next;
+}graph_node;
 
 
 /*
  graph node struct definition
  */
-typedef struct g_node{
+/*typedef struct g_node{
 	list_node *node;
 	list_node *tail;
-}graph_node;
+}graph_node;*/
 
 
 /*
@@ -32,11 +32,14 @@ typedef struct g{
 
 graph *graph_init(int nodes); // DONE
 graph_node *create_graph_node(int new_value); // DONE
-list_node *create_list_node(int new_value); // DONE
+//list_node *create_list_node(int new_value); // DONE
 graph *graph_add_node(graph *gr, int node_index, int new_value); // DONE
-void depth_first_search(graph *gr);
+
+void depth_first_search(graph *gr, int node_index);
 void breadth_first_search(graph *gr);
 void dijkstra(graph *gr);
 void bellman_ford(graph *gr);
+
+void print_graph(graph *gr);
 
 #endif
